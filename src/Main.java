875f91be2c823bc092f48f4bcf2c712da4add440
BartoszM66 <bartosz.mazurkiewicz@vp.pl>
2026-02-24 14:45:27 +0100

    Refactored to Repository pattern

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Locale;
 
-class Expense {
-    private String description;
-    private double amount;
-    private String category;
-
-    public Expense(String description, double amount, String category) {
-        this.description = description;
-        this.amount = amount;
-        this.category = category;
-    }
-
-    public double getAmount(){
-        return amount;
-    }
-
-    @Override
-    public String toString(){
-        String sformatowanaKwota = String.format("%.2f", amount);
-        return "Opis: " + description + " | Kwota: " + sformatowanaKwota + " | Kategoria: " + category;
-    }
-}
-
-class ExpenseManager{
-    private List<Expense> expenses = new ArrayList<>();
-
-    public void addExpense(Expense expense){
-        expenses.add(expense);
-    }
-
-    public void displayAllExpenses(){
-        for(Expense expense : expenses){
-            System.out.println(expense);
-        }
-    }
-
-    public double sumTotalExpenses(){
-        double total = 0;
-        for(Expense expense : expenses){
-            total += expense.getAmount();
-        }
-        return total;
-    }
-}
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-        ExpenseManager expenseManager = new ExpenseManager();
+        ExpenseRepository repository = new InMemoryExpenseRepository();
+        ExpenseManager expenseManager = new ExpenseManager(repository);
 
         while(true){
             System.out.println("Podaj opis (lub 'X' aby zakończyć): ");
