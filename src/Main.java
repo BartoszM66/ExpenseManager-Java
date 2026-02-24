f709c0823e27c2bbd1b00642ae88debb60f97449
BartoszM66 <bartosz.mazurkiewicz@vp.pl>
2026-02-24 14:24:45 +0100

    Fixed input and output format

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Locale;
 
 class Expense {
     private String description;
@@ -19,7 +20,8 @@ class Expense {
 
     @Override
     public String toString(){
-        return "Opis: " + description + " | Kwota: " + amount + " | Kategoria: " + category;
+        String sformatowanaKwota = String.format("%.2f", amount);
+        return "Opis: " + description + " | Kwota: " + sformatowanaKwota + " | Kategoria: " + category;
     }
 }
 
@@ -46,22 +48,29 @@ class ExpenseManager{
 }
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         ExpenseManager expenseManager = new ExpenseManager();
 
         while(true){
-            System.out.println("Podaj opis: ");
+            System.out.println("Podaj opis (lub 'X' aby zakończyć): ");
             String opis = sc.nextLine();
-            if(opis.equals("X")){
+            if(opis.equalsIgnoreCase("X")){
                 break;
             }
             System.out.println("Podaj kwotę: ");
-            double kwota = sc.nextDouble();
+            String kwotaTekst = sc.nextLine();
+            double kwota = Double.parseDouble(kwotaTekst.replace(',','.'));
             System.out.println("Podaj kategorię: ");
             String kategoria = sc.nextLine();
 
             expenseManager.addExpense(new Expense(opis, kwota, kategoria));
+            System.out.println("--- Dodano wydatek ---");
         }
 
+        System.out.println("--- Lista wydatków ---");
+        expenseManager.displayAllExpenses();
+        System.out.println("\n--- Całkowita kwota ---");
+        double suma = expenseManager.sumTotalExpenses();
+        System.out.println(String.format(Locale.US,"Suma: %.2f PLN", suma));
     }
 }
